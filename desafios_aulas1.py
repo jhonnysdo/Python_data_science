0f64607f5ebe0ac721e81a3aff9b25a701d9a423
Jhonny Santos <jhonny.sdo@hotmail.com>
2025-01-14 20:54:12 -0300

    Create desafios_aulas1.py
    Praticando com os desafios sugeridos no curso, bloco de aulas 1

---
+++ desafios_aulas1.py
@@ -0,0 +1,22 @@
+##Imprima a frase Escola de Dados da Alura!.
+print ('Escola de dados da Alura')
+##Imprima seu nome e seu sobrenome seguindo a estrutura abaixo:
+##Nome: [seu nome]
+##Sobrenome: [seu sobrenome]
+print ('Nome: Jhonny')
+print ('Sobrenome: Santos')
+
+## Imprima o seu primeiro nome letra a letra. Por exemplo, meu nome é Mirla, então eu obtenho a seguinte saída:
+print ('J')
+print ('H')
+print ('O')
+print ('N')
+print ('N')
+print ('Y')
+
+## Imprima o dia do seu nascimento em formato dia mês ano. Lembrando que os valores de dia e ano não podem estar entre aspas. Supondo uma data de aniversário dia 28 de fevereiro de 2003, o formato deve estar como no exemplo abaixo
+
+print (24, 'junho', 1995)
+
+## Imprima, em um único print, o atual ano que você está fazendo esse curso. O valor do ano deve ser um dado numérico e a saída do print deve ser a seguinte
+print ('Ano atual do curso:', 2025)
